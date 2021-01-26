e4f3c8a5ffd1cd67fc62d3eeeb8f1bff27ed87e6
harbar20 <harbar2021@gmail.com>
2021-01-26 18:35:32 -0500

    added patreon support i think

--- server.js
+++ server.js
@@ -1,15 +1,62 @@
 const express = require("express");
+const patreon = require("patreon");
 const request = require("request");
+
+const patreonAPI = patreon.patreon;
+const patreonOAuth = patreon.oauth;
+const patreonOAuthClient = patreonOAuth(
+	process.env.PATREON_CLIENT_ID,
+	process.env.PATREON_CLIENT_SECRET
+);
+//Redirect for Patreon OAuth
+app.get("/patreon-redirect", async (req, res) => {
+	const code = req.query.code;
+	const discordID = req.query.state;
+
+	console.log("I'm here 1!");
+
+	await patreonOAuthClient
+		.getTokens(code, "https://kills.porygonbot.xyz/patreon-redirect")
+		.then(async (response) => {
+			console.log("I'm here 2!");
+
+			const patreonAPIClient = patreonAPI(response.access_token);
+			return patreonAPIClient("/current_user");
+		})
+		.then(async (result) => {
+			console.log("I'm here 3!");
+
+			const store = result.store;
+			const user = store.findAll("user").map((user) => user.serialize());
+			let newData = {};
+			newData[discordID] = user;
+
+			let response = await request({
+				url: `https://jsonbase.com/PorygonBot/patreon-user`,
+				method: "PUT",
+				headers: { "content-type": "application/json" },
+				body: JSON.stringify(newData),
+			});
+
+			res.end("Done, baby!");
+		})
+		.catch((err) => {
+			console.log("I'm here error!");
+			console.error(err);
+			res.end(err);
+		});
+});
 
 let app = express();
 
 app.use(express.text());
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
 	res.send(
 		`This is <a href="https://github.com/PorygonBot/kills-site">PorygonBot/kills-site</a> 's home.`
 	);
 });
+
 
 app.post("/:id", async (req, res) => {
 	let response = await request({
