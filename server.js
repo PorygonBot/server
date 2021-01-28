1711e83bce416fe2412816e95538e29438324ae9
harbar20 <harbar2021@gmail.com>
2021-01-27 19:28:57 -0500

    changed everything to manual like theo said

--- server.js
+++ server.js
@@ -1,91 +1,89 @@
 const express = require("express");
-const patreon = require("patreon");
 const request = require("request");
+const qs = require("querystring");
 
-const patreonAPI = patreon.patreon;
-const patreonOAuth = patreon.oauth;
-const patreonOAuthClient = patreonOAuth(
-	process.env.PATREON_CLIENT_ID,
-	process.env.PATREON_CLIENT_SECRET
-);
 let app = express();
-
-app.use(express.json());
-
-//Redirect for Patreon OAuth
-app.get("/patreon-redirect", async (req, res) => {
-	const code = req.query.code;
-	const discordID = req.query.state;
-
-	console.log(code + "     " + discordID);
-	console.log("I'm here 1!");
-
-	await patreonOAuthClient
-		.getTokens(code, "https://kills.porygonbot.xyz/patreon-redirect")
-		.then(async (response) => {
-			console.log("I'm here 2!");
-			console.log(response);
-
-			const patreonAPIClient = patreonAPI(response.access_token);
-			return patreonAPIClient("/current_user");
-		})
-		.then(async (result) => {
-			console.log("I'm here 3!");
-			console.log(result);
-
-			const store = result.store;
-			const user = store.findAll("user").map((user) => user.serialize());
-			console.log(user);
-			let newData = {};
-			newData[discordID] = user;
-
-			let response = await request({
-				url: `https://jsonbase.com/PorygonBot/patreon-user`,
-				method: "PUT",
-				headers: { "content-type": "application/json" },
-				body: JSON.stringify(newData),
-			});
-
-			res.end("Done, baby!");
-		})
-		.catch((err) => {
-			console.log("I'm here error!");
-			console.error(err);
-		});
-});
 
 app.use(express.text());
 
+//Redirect for Patreon OAuth
+app.get("/patreon-redirect", async (req, res) => {
+    const code = req.query.code;
+    const discordID = req.query.state;
+
+    //Exchange Token
+    const data = qs.stringify({
+        code: code,
+        grant_type: "authorization_code",
+        client_id: process.env.PATREON_CLIENT_ID,
+        client_secret: process.env.PATREON_CLIENT_SECRET,
+        redirect_uri: "https://kills.porygonbot.xyz/patreon-redirect",
+    });
+    const newRes = request({
+        url: `https://www.patreon.com/api/oauth2/token?${data}`,
+        method: "POST",
+        headers: {
+            Authorization: "Bearer ",
+        },
+    });
+    const access_token = newRes.data.access_token;
+
+    //Making the Patreon request
+    const newData = qs.stringify({
+        fields: { member: "patron_status", user: "full_name" },
+        include: "memberships",
+    });
+    const userRes = request({
+        url: `https://www.patreon.com/api/oauth2/v2/identity?${newData}`,
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${access_token}`,
+        },
+    });
+
+    //Posting to JSONBase API
+    const baseData = {};
+    baseData[discordID] = userRes.data.included[0];
+    let baseRes = request({
+        url: `https://jsonbase.com/PorygonBot/patreon-user`,
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(baseData),
+    });
+
+    res.send("Done! You may now go back to Discord.");
+});
+
 //Home page
 app.get("/", function (req, res) {
-	res.send(
-		`This is <a href="https://github.com/PorygonBot/kills-site">PorygonBot/kills-site</a> 's home for kills histories.`
-	);
+    res.send(
+        `This is <a href="https://github.com/PorygonBot/kills-site">PorygonBot/kills-site</a> 's home for kills histories.`
+    );
 });
 
 //When bot posts
 app.post("/:id", async (req, res) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	let response = await request({
-		url: `https://jsonbase.com/PorygonBot/${req.params.id}`,
-		method: "PUT",
-		headers: { "content-type": "application/json" },
-		body: JSON.stringify({ msg: req.body }),
-	});
-	res.send({ status: 200, id: req.params.id });
+    res.header("Access-Control-Allow-Origin", "*");
+    let response = request({
+        url: `https://jsonbase.com/PorygonBot/${req.params.id}`,
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ msg: req.body }),
+    });
+    res.send({ status: 200, id: req.params.id });
 });
 
 //When people get
 app.get("/:id", async (req, res) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	if (req.params.id !== "favicon.ico") {
-		let message = request.get(
-			`https://jsonbase.com/PorygonBot/${req.params.id}`,
-			(err, response, body) => {
-				res.send(JSON.parse(body).msg);
-			}
-		);
-	}
+    res.header("Access-Control-Allow-Origin", "*");
+    if (req.params.id !== "favicon.ico") {
+        let message = request.get(
+            `https://jsonbase.com/PorygonBot/${req.params.id}`,
+            (err, response, body) => {
+                res.send(JSON.parse(body).msg);
+            }
+        );
+    }
 });
 
 app.listen(process.env.PORT);
